49445c2ef0351f392185e7df96f69aed6eb3a882
Daniel Eastburn <danieleastburnWork@gmail.com>
2022-09-17 20:18:20 +0100

    Add Tasks

--- 01ReactCrashCourse/react-task-tracker/src/App.js
+++ 01ReactCrashCourse/react-task-tracker/src/App.js
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 function App() {
@@ -27,11 +28,33 @@ function App() {
     },
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task) => task.id !== id)) //Set tasks to filtered tasks
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
+    )
+    console.log(id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks = {tasks}/>
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>):('No Tasks To Show')}
     </div>
+    //turnery ? : if then
   );
 }
 
