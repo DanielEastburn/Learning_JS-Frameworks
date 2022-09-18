0504a7f4658f5fac00cb6fb77c6ceee5da08da89
Daniel Eastburn <danieleastburnWork@gmail.com>
2022-09-19 00:56:42 +0100

    React Crash Course Complete

--- 01ReactCrashCourse/react-task-tracker/src/App.js
+++ 01ReactCrashCourse/react-task-tracker/src/App.js
@@ -1,8 +1,11 @@
 // import React from 'react' - for class
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 
 function App() {
@@ -55,7 +58,18 @@ function App() {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const TaskToToggle = await fetchTasks(id)
+    const updTask = {...TaskToToggle, reminder: !TaskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+    
     setTasks(
       tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
     )
@@ -63,13 +77,18 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>):('No Tasks To Show')}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask} />}
+        {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/>):('No Tasks To Show')}
+        {/* <Route path='/about' component={About}/> */}
+        <Footer />
+      </div>
+    </Router>
+
     //turnery ? : if then
-  );
+  )
 }
 
 // class App extends React.Component {
